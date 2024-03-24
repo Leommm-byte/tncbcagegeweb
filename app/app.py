3cbd686c8fc9a910c17466c1df185abb20b7b994
Leommm-byte <leom20031@gmail.com>
2024-03-24 11:55:17 +0100

    changed napp to app

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from .website import create_app
 
 if __name__ == "__main__":
-    napp = create_app()
-    napp.run(debug=True, host='0.0.0.0')
+    app = create_app()
+    app.run(debug=True, host='0.0.0.0')
