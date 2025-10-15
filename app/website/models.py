54f36c8f313c5356da56626afc00327ae826e7a2
Leommm-byte <leom20031@gmail.com>
2025-10-15 21:08:25 +0100

    Updated User model in models.py to rename password field to password_hash and reintroduced email_id field as mandatory.

--- app/website/models.py
+++ app/website/models.py
@@ -16,6 +16,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    # email_id = db.Column(db.String(100), index=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    email_id = db.Column(db.String(100), nullable=False)
+
 
